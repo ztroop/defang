edd8e7e69619a5fbd912b2ad85ec8c75e2df4579
Zackary Troop <zack.tro@gmail.com>
2021-11-15 00:05:09 -0500

    Code review

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
 use clap::{crate_authors, crate_version, App, Arg};
 
-const SUBS: [(&str, &str); 3] = [(".", "[.]"), ("http", "hxxp"), ("ftp", "fxp")];
+const SUBS: [(&str, &str); 4] = [
+    (".", "[.]"),
+    ("@", "[at]"),
+    ("ftp", "fxp"),
+    ("http", "hxxp"),
+];
 
-fn reverse_tuple<'a>(pair: (&'a str, &'a str)) -> (&'a str, &'a str) {
+enum Order {
+    Normal,
+    Reversed,
+}
+
+fn reverse_tuple<T, U>(pair: (T, U)) -> (U, T) {
     let (first, second) = pair;
     (second, first)
 }
 
-fn replace(mut url: String, s: [(&str, &str); 3]) -> String {
+fn replace(mut url: String, s: [(&str, &str); 4]) -> String {
     for item in s.iter() {
         if url.contains(item.0) {
             url = url.replace(item.0, item.1);
@@ -16,12 +26,11 @@ fn replace(mut url: String, s: [(&str, &str); 3]) -> String {
     url
 }
 
-fn fang(url: &str, reversed: bool) -> String {
-    let url = String::from(url.trim());
-    if reversed {
-        replace(url, SUBS.clone().map(|x| reverse_tuple(x)))
-    } else {
-        replace(url, SUBS)
+fn fang(url: &str, order: Order) -> String {
+    let url = url.trim().to_owned();
+    match order {
+        Order::Reversed => replace(url, SUBS.map(|x| reverse_tuple(x))),
+        Order::Normal => replace(url, SUBS),
     }
 }
 
@@ -51,12 +60,12 @@ fn main() {
         .get_matches();
 
     match matches.value_of("defang") {
-        Some(i) => println!("{}", fang(i, false)),
+        Some(i) => println!("{}", fang(i, Order::Normal)),
         None => (),
     };
 
     match matches.value_of("refang") {
-        Some(i) => println!("{}", fang(i, true)),
+        Some(i) => println!("{}", fang(i, Order::Reversed)),
         None => (),
     };
 }
@@ -67,22 +76,34 @@ mod tests {
 
     #[test]
     fn test_defang() {
-        let defanged_google: String = fang("http://google.com", false);
-        assert_eq!(defanged_google, String::from("hxxp://google[.]com"));
+        let defanged_google: String = fang("http://google.com", Order::Normal);
+        assert_eq!(defanged_google, "hxxp://google[.]com".to_owned());
 
-        let defanged_debian: String = fang("ftp://ftp.debian.org/debian", false);
+        let defanged_debian: String = fang("ftp://ftp.debian.org/debian", Order::Normal);
         assert_eq!(
             defanged_debian,
-            String::from("fxp://fxp[.]debian[.]org/debian")
-        )
+            "fxp://fxp[.]debian[.]org/debian".to_owned()
+        );
+
+        let defanged_email: String = fang("bad.actor@example.com", Order::Normal);
+        assert_eq!(defanged_email, "bad[.]actor[at]example[.]com".to_owned())
     }
 
     #[test]
     fn test_refang() {
-        let defanged_google: String = fang("hxxp://google[.]com", true);
-        assert_eq!(defanged_google, String::from("http://google.com"));
+        let refanged_google: String = fang("hxxp://google[.]com", Order::Reversed);
+        assert_eq!(refanged_google, "http://google.com".to_owned());
 
-        let defanged_debian: String = fang("fxp://fxp[.]debian[.]org/debian", true);
-        assert_eq!(defanged_debian, String::from("ftp://ftp.debian.org/debian"))
+        let refanged_debian: String = fang("fxp://fxp[.]debian[.]org/debian", Order::Reversed);
+        assert_eq!(refanged_debian, "ftp://ftp.debian.org/debian".to_owned());
+
+        let refanged_email: String = fang("bad[.]actor[at]example[.]com", Order::Reversed);
+        assert_eq!(refanged_email, "bad.actor@example.com".to_owned())
+    }
+
+    #[test]
+    fn test_reverse_tuple() {
+        assert_eq!(reverse_tuple((0, 1)), (1, 0));
+        assert_eq!(reverse_tuple(("foo", "bar")), ("bar", "foo"))
     }
 }
